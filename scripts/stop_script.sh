9aaf20bada0abcbd4d7513143ecc9dd07c50d77c
pankaj779 <84573170+pankaj779@users.noreply.github.com>
2022-06-21 12:09:50 +0530

    Update stop_script.sh

--- scripts/stop_script.sh
+++ scripts/stop_script.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 cd /home/ubuntu/node-express-realworld-example-app
-pm2 stop app.js
+
+# this script is to check whether the pm2 app is srnning or not
+echo "running the script"
+
+pm2 describe app > /dev/null
+RUNNING=$?
+
+if [ "${RUNNING}" -ne 0 ]; then
+        echo "App is not running"
+else
+        echo "APP IS RUNNING"
+        echo "APP WILL BE TERMINIATED"
+        sudo pm2 stop all
+fi;
+
