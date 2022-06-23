c5dba9acd4a6965a88d2be6fcc329d36762a74be
pankaj779 <84573170+pankaj779@users.noreply.github.com>
2022-06-23 10:56:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ if (!isProduction) {
 if(isProduction){
   mongoose.connect(process.env.MONGODB_URI);
 } else {
-  mongoose.connect(process.env.PANKAJ_URI);
+  mongoose.connect('pankaj');
   mongoose.set('debug', true);
 }
 
